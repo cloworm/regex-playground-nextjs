e92c506ec705e760fec2a42264bec60769d0b698
cloworm <chloe.echikson@gmail.com>
2020-12-26 17:05:52 -0500

    implement the app logic

--- src/components/RegexPlayground.tsx
+++ src/components/RegexPlayground.tsx
@@ -1,99 +1,31 @@
-import React, { FunctionComponent, CSSProperties, useState, useCallback } from 'react'
-import TextField from '@material-ui/core/TextField'
-import Fab from '@material-ui/core/Fab'
-// import ContentAdd from '@material-ui/core/svg-icons/content/add'
-// import ContentRemove from '@material-ui/core/svg-icons/content/remove'
-import Button from '@material-ui/core/Button'
-// import MatchBox from './MatchBox.jsx'
-// import RegexReference from './RegexReference.jsx'
-// import Query from './Query.js'
-// import Share from './Share.jsx'
-import AppBar from '@material-ui/core/AppBar'
-// import ShareLink from './ShareLink.js'
-import IconButton from '@material-ui/core/IconButton'
+import React, { FunctionComponent, useState, useCallback } from 'react'
 
-const styles = {
-  actionButton: {
-    float: 'right',
-    marginRight: '10px',
-  } as CSSProperties,
-  appBar: {
-    backgroundColor: '#fff'
-  } as CSSProperties,
-  clearButton: {
-    marginBottom: '10px',
-    verticalAlign: 'bottom'
-  } as CSSProperties,
-  flagsField: {
-    float: 'left',
-    width: '100%'
-  } as CSSProperties,
-  flagsFieldSpan: {
-    display: 'inline-block',
-    position: 'relative'
-  } as CSSProperties,
-  floatingLabel: {
-    color: '#000'
-  } as CSSProperties,
-  footer: {
-    backgroundColor: '#e91e63',
-    color: '#fff',
-    lineHeight: '50px',
-    height: '50px',
-    marginBottom: '0'
-  } as CSSProperties,
-  github: {
-    height: '1rem'
-  } as CSSProperties,
-  iconFill: {
-    fill: '#000'
-  } as CSSProperties,
-  link: {
-    color:'#fff',
-    textDecoration: 'none',
-    textShadow: 'none',
-  } as CSSProperties,
-  logo: {
-    padding: '2px',
-    marginLeft: '10px'
-  } as CSSProperties,
-  matchBoxMargin: {
-    margin: '15px'
-  } as CSSProperties,
-  patternField: {
-    float: 'left',
-    width: '100%'
-  } as CSSProperties,
-  slashLeft: {
-    position: 'absolute',
-    top: '42px',
-    left: '-6px'
-  } as CSSProperties,
-  slashRight: {
-    position: 'absolute',
-    top: '42px',
-    right: '-6px'
-  } as CSSProperties,
-  slashWrapper: {
-    display: 'inline-block',
-    position: 'relative',
-    marginRight: '12px',
-    width: '100%'
-  } as CSSProperties,
-  title: {
-    fontFamily: 'Montserrat, sans-serif',
-    color: '#e91e63',
-  } as CSSProperties,
-  underline: {
-    borderColor: '#000'
-  } as CSSProperties,
-  underlineFocus: {
-    borderColor: '#fff'
-  } as CSSProperties
-};
+interface MatcherBoxProps {
+  onChange: (value: string) => void
+  pattern: RegExp,
+  value: string,
+}
+
+const MatcherBox: FunctionComponent<MatcherBoxProps> = ({
+  onChange,
+  pattern,
+  value,
+}) => {
+  const handleChange = useCallback((event) => {
+    onChange(event.target.value)
+  }, [onChange])
+
+  return <div>
+    <input value={value} onChange={handleChange} />
+    {/* TODO: Properly show the matches */}
+    {/* TODO: List out the match-groups? */}
+    {value.match(pattern)}
+  </div>
+}
 
 const RegexPlayground: FunctionComponent = () => {
   const [pattern, setPattern] = useState<string>('')
+  const [matchers, setMatchers] = useState<string[]>([''])
   const [flags, setFlags] = useState<string>('')
 
   const handlePatternChange = useCallback((event) => {
@@ -118,7 +50,7 @@ const RegexPlayground: FunctionComponent = () => {
 
   return (
     <div>
-      <AppBar
+      {/* <AppBar
         title="RegEx Playground"
         // titleStyle={styles.title}
         style={styles.appBar}
@@ -129,45 +61,45 @@ const RegexPlayground: FunctionComponent = () => {
             {/* <Share
               pattern={pattern}
               flags={flags}
-              matches={matchBoxValues}
-            /> */}
+              matches={matchers}
+            /> * /}
           </div>
         </div>
-      </AppBar>
+      </AppBar> */}
       <div className='container-fluid'>
         <div className='row'>
           <div className='col-xs-12 col-md-8'>
             <div className='container-fluid'>
               <div className='row bottom-xs'>
                 <div className='col-xs-8 col-sm-6'>
-                  <span style={styles.slashWrapper}>
-                    <span style={styles.slashLeft}>/</span>
-                    <TextField
+                  <span>
+                    <span>/</span>
+                    <input
                       autoCapitalize='off'
                       autoCorrect='off'
                       // floatingLabelText='Pattern'
                       // floatingLabelStyle={styles.floatingLabel}
                       value={pattern}
                       onChange={handlePatternChange}
-                      style={styles.patternField}
+                      // style={styles.patternField}
                       // ref='pattern'
                       // errorText={errorMessage}
                       // underlineStyle={styles.underline}
                       // underlineFocusStyle={styles.underlineFocus}
                     />
-                    <span style={styles.slashRight}>/</span>
+                    <span>/</span>
                   </span>
                 </div>
                 <div className='col-xs-4 col-sm-4'>
-                  <span style={styles.flagsFieldSpan}>
-                    <TextField
+                  <span>
+                    <input
                       autoCapitalize='off'
                       autoCorrect='off'
                       // floatingLabelText='Flags'
                       // floatingLabelStyle={styles.floatingLabel}
                       value={flags}
                       onChange={handleFlagsChange}
-                      style={styles.flagsField}
+                      // style={styles.flagsField}
                       // ref='flags'
                       // underlineStyle={styles.underline}
                       // underlineFocusStyle={styles.underlineFocus}
@@ -175,40 +107,66 @@ const RegexPlayground: FunctionComponent = () => {
                   </span>
                 </div>
                 <div className='col-sm-2 col-xs-12'>
-                  <Button
-                    // label='Clear'
-                    style={styles.clearButton}
+                  <button
                     onClick={handleClickClear}
                   >
                     Clear
-                  </Button>
+                  </button>
                 </div>
               </div>
             </div>
             <br />
-            {/* {this.renderMatchBoxes(re)} */}
-            {/* <Fab
-              backgroundColor='#fff'
-              mini={true}
-              style={styles.actionButton}
-              onClick={this.handleNewMatchBox}
-              iconStyle={styles.iconFill}
+            {
+              matchers.map((value, i) => {
+                return (
+                  <div key={i}>
+                    {/* <MatchBox
+                      pattern={re}
+                      value={value}
+                      onChange={this.handleMatchBoxChange.bind(null, i)}
+                    /> */}
+                    <MatcherBox
+                      pattern={re}
+                      value={value}
+                      onChange={(value) => {
+                        setMatchers((prevMatchers) => {
+                          return [
+                            ...prevMatchers.slice(0, i),
+                            value,
+                            ...prevMatchers.slice(i + 1),
+                          ]
+                        })
+                      }}
+                    />
+                  </div>
+                )
+              })
+            }
+            <button role='button'
+              onClick={() => {
+                setMatchers((prevMatchers) => {
+                  return [
+                    ...prevMatchers,
+                    '',
+                  ]
+                })
+              }}
             >
-              {/* <ContentAdd /> * /}
               +
-            </Fab>
-            {(matchBoxValues.length > 1) ?
-              <Fab
-                backgroundColor='#bdc3c7'
-                mini={true}
-                style={styles.actionButton}
-                onClick={this.handleRemoveMatchBox}
-                iconStyle={styles.iconFill}
+            </button>
+            {
+              matchers.length > 1 && <button role='button'
+                onClick={() => {
+                  setMatchers((prevMatchers) => {
+                    return [
+                      ...prevMatchers.slice(0, prevMatchers.length - 1),
+                    ]
+                  })
+                }}
               >
-                {/* <ContentRemove /> * /}
                 -
-              </Fab> : null
-            } */}
+              </button>
+            }
           </div>
           <div className='col-xs-12 col-md-4'>
             {/* <RegexReference
@@ -217,9 +175,15 @@ const RegexPlayground: FunctionComponent = () => {
           </div>
         </div>
       </div>
-      <footer style={styles.footer}>
+      <footer>
         <div className='container-fluid'>
-          <a style={styles.link} href='http://www.github.com/cloworm' target='_blank'><img style={styles.github} src='github.png' /> cloworm</a> © 2016
+          <a href='http://www.github.com/cloworm' target='_blank' rel='noopener noreferrer'>
+            <img src='/github.png' />
+            { ' ' }
+            cloworm
+          </a>
+          { ' ' }
+          © 2020
         </div>
       </footer>
     </div>
