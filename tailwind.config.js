daa3282b44f72ea35aacd3657c47db1b2557a0be
cloworm <chloe.echikson@gmail.com>
2020-12-30 15:20:00 -0500

    fix purge path: ./src/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
