c15a43517a733381b698eec7eee5f642242fab43
cloworm <chloe.echikson@gmail.com>
2020-12-30 14:15:07 -0500

    fix jestconfig src/tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // The root of your source code, typically /src
   // `<rootDir>` is a token Jest substitutes
-  roots: ['<rootDir>'],
+  roots: ['<rootDir>/src'],
 
   // Jest transformations -- this adds support for TypeScript
   // using ts-jest
@@ -22,7 +22,7 @@ module.exports = {
   // Test spec file resolution pattern
   // Matches parent folder `__tests__` and filename
   // should contain `test` or `spec`.
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '(/src/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
 
   // Module file extensions for importing
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
