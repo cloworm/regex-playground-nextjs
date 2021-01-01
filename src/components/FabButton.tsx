76c14b8caeae8ad49ac9abc007506af7e0e04304
Chloe Echikson <chloe.echikson@gmail.com>
2021-01-01 18:19:12 -0500

    Jest tests for match UI functionality (#6)
    * expect router.push is called when input changes
    
    * fix no match boxes showing when empty matches query param
    
    * expect specific match group values

--- src/components/FabButton.tsx
+++ src/components/FabButton.tsx
@@ -2,12 +2,20 @@ import React, { FunctionComponent } from 'react'
 
 interface Props {
   onClick: () => void
+  name?: string
 }
 
-const FabButton: FunctionComponent<Props> = ({ children, onClick }) => {
+const FabButton: FunctionComponent<Props> = ({ children, name, onClick }) => {
   return (
-    <button className="w-10 h-10 p-2 rounded-full bg-gradient-to-br to-theme_slateBlue from-theme_hotPink shadow" onClick={onClick}>
+    <button
+      role='button'
+      type='button'
+      onClick={onClick}
+      name={name}
+      className="w-10 h-10 p-2 rounded-full bg-gradient-to-br to-theme_slateBlue from-theme_hotPink shadow"
+    >
       {children}
+      <span className='hidden'>{name}</span>
     </button>
   )
 }
