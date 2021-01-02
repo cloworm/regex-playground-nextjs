b723aab989defe9bd8d10190762fdcd17fe94379
cloworm <chloe.echikson@gmail.com>
2021-01-01 21:08:51 -0500

    lint

--- src/tests/Index.test.tsx
+++ src/tests/Index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, fireEvent, within } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { useRouter } from 'next/router'
 import { UrlObject } from 'url'
