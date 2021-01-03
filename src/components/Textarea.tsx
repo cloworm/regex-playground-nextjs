d05831746c0c1eb96bc4ba0796741711ca07d0c8
cloworm <chloe.echikson@gmail.com>
2021-01-03 14:36:03 -0500

    remove unused import

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useCallback } from 'react'
+import React, { FunctionComponent } from 'react'
 import { HighlightWithinTextarea } from 'react-highlight-within-textarea'
 
 interface Props {
